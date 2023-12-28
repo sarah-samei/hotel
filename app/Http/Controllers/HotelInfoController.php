e2291cd2b55e2c29370e9e18ae49d6a2a5a8114e
sarah <sarah.00.samei@gmail.com>
2023-12-28 17:06:26 +0330

    Create HotelInfo model and migration and controller.

---
+++ app/Http/Controllers/HotelInfoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HotelInfo;
+use Illuminate\Http\Request;
+
+class HotelInfoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(HotelInfo $hotelInfo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(HotelInfo $hotelInfo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, HotelInfo $hotelInfo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(HotelInfo $hotelInfo)
+    {
+        //
+    }
+}
