9cf1710d84dca67c65507569cddbede0ae9d352f
sarah <sarah.00.samei@gmail.com>
2023-12-29 23:31:04 +0330

    Add links to bootstrap.

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,9 +3,16 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="{{asset('dist/css/bootstrap.min.css')}}">
 	<title></title>
 </head>
 <body>
 	@yield("content")
+
+
+	
+	<script src="{{asset('dist/js/jquery-3.7.1.min.js')}}"></script>
+	<script src="{{asset('dist/js/popper.min.js')}}"></script>
+	<script src="{{asset('dist/js/bootstrap.min.js')}}"></script>
 </body>
 </html>
