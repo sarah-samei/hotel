1cc7409995cfd6da72520421ca929e9a855082ae
sarah <sarah.00.samei@gmail.com>
2023-12-29 22:40:53 +0330

    Add images to index file.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,5 +12,11 @@
 
 
 	{{--<i>{{ $guest[0]->fname}}</i>--}}
+<div>
+	<img src="dist/img/hotel1.jpg" style="width:350px; height: 350px; border-radius: 50%; border: 5px dashed black">
+	<img src="dist/img/hotel2.jpg" style="width:350px; height: 350px; border-radius: 50%; border: 5px dashed black;">
+	<img src="dist/img/hotel3.jpg" style="width:350px; height: 350px; border-radius: 50%; border: 5px dashed black">
+	<img src="dist/img/hotel4.jpg" style="width:350px; height: 350px; border-radius: 50%; border: 5px dashed black">
+</div>
 
 @endsection
