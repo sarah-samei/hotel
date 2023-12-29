a8f91dd919c001b441d3cf5a6a3261b6225f4ef5
sarah <sarah.00.samei@gmail.com>
2023-12-29 20:54:02 +0330

    Create table hotel_infos.

--- database/migrations/2023_12_28_133151_create_hotel_infos_table.php
+++ database/migrations/2023_12_28_133151_create_hotel_infos_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('hotel_infos', function (Blueprint $table) {
             $table->id();
+            $table->string('fname');
+            $table->string('lname');
+            $table->string('room_number');
             $table->timestamps();
         });
     }
